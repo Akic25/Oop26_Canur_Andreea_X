c0d68fcc7b7bc53c8805a7b58311f2fa351a30ea
AndreeaCanur <andcanur@gmail.com>
2026-02-24 22:05:35 +0200

    Update Globals.h

--- lab2.2/Globals.h
+++ lab2.2/Globals.h
@@ -3,8 +3,8 @@
 #include"Grades.h"
 
 int CompareStudentName(Grades s1, Grades s2);
-int CompareMathGrade(Grades s1, Grades s2);
-int CompareHistoryGrade(Grades s1, Grades s2);
+int CompareMathGrade(Grades* s1, Grades* s2);
+int CompareHistoryGrade(Grades& s1, Grades& s2);
 int CompareEnglishGrade(Grades s1, Grades s2);
 int CompareAverageGrade(Grades s1, Grades s2);
 
