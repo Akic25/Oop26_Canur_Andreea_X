cf49d7a4ac20284cb9188e334a998433b11f9f1f
AndreeaCanur <andcanur@gmail.com>
2026-03-24 17:41:34 +0200

    problema 6

---
+++ Laboratoru6/BMW.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Car.h"
+class BMW : public Car
+{
+public:
+	BMW(); //constructor simplu toate masinile vor avea variabilele fixate
+	double GetFuelCapacity() override;
+	double GetFuelConsumption() override;
+	double GetSpeed(Weather condition) override;
+	double  GetRaceTiming(double circuitLength, Weather condition) override;
+	std::string GetName() override;
+};
---
+++ Laboratoru6/Volvo.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Car.h"
+class Volvo : public Car
+{
+public:
+	Volvo(); //constructor simplu toate masinile vor avea variabilele fixate
+	double GetFuelCapacity() override;
+	double GetFuelConsumption() override;
+	double GetSpeed(Weather condition) override;
+	double  GetRaceTiming(double circuitLength, Weather condition) override;
+	std::string GetName() override;
+};
